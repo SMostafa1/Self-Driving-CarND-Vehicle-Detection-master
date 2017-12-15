93ff4fc97cb93e9ceec9860d90b9dad7c090bb51
Sara <33544426+SMostafa1@users.noreply.github.com>
2017-12-15 12:36:18 +0200

    Add files via upload

---
+++ MAIN.py
@@ -0,0 +1,153 @@
+################################################Imports#################################################################
+import glob
+import Utilities
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.cross_validation import train_test_split
+import time
+from sklearn.svm import LinearSVC
+from scipy.ndimage.measurements import label
+import os
+from moviepy.editor import *
+########################################################################################################################
+
+# Read in cars and notcars
+car_images = glob.glob('training_dataset/vehicles/**/*.png')
+noncar_images  = glob.glob('training_dataset/non-vehicles/**/*.png')
+
+print(len(car_images), len(noncar_images))
+
+car_img = mpimg.imread(car_images[5])
+_, car_dst = Utilities.get_hog_features(car_img[:,:,2], 9, 8, 8, vis=True, feature_vec=True)
+
+noncar_img = mpimg.imread(noncar_images[5])
+_, noncar_dst = Utilities.get_hog_features(noncar_img[:,:,2], 9, 8, 8, vis=True, feature_vec=True)
+
+# Visualize
+f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(7,7))
+f.subplots_adjust(hspace = .4, wspace=.2)
+ax1.imshow(car_img)
+ax1.set_title('Car Image', fontsize=16)
+ax2.imshow(car_dst, cmap='gray')
+ax2.set_title('Car HOG', fontsize=16)
+ax3.imshow(noncar_img)
+ax3.set_title('Non-Car Image', fontsize=16)
+ax4.imshow(noncar_dst, cmap='gray')
+ax4.set_title('Non-Car HOG', fontsize=16)
+plt.show()
+
+
+
+# ########################################Feature extraction parameters#################################################
+colorspace = 'YCrCb' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 11
+pix_per_cell = 16
+cell_per_block = 2
+hog_channel = 'ALL' # Can be 0, 1, 2, or "ALL"
+
+car_features = Utilities.extract_features(car_images, cspace=colorspace, orient=orient,
+                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                        hog_channel=hog_channel)
+notcar_features = Utilities.extract_features(noncar_images, cspace=colorspace, orient=orient,
+                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                        hog_channel=hog_channel)
+
+X = np.vstack((car_features, notcar_features)).astype(np.float64)
+########################################################################################################################
+
+################################################Apply scalar and define labels##########################################
+# Fit a per-column scaler
+X_scaler = StandardScaler().fit(X)
+# Apply the scaler to X
+scaled_X = X_scaler.transform(X)
+# Define the labels vector
+y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+########################################################################################################################
+
+#####################################Split data into train and test data################################################
+# Split up data into randomized training and test sets
+rand_state = np.random.randint(0, 100)
+X_train, X_test, y_train, y_test = train_test_split(
+    scaled_X, y, test_size=0.2, random_state=rand_state)
+
+print('Using:',orient,'orientations',pix_per_cell,
+    'pixels per cell and', cell_per_block,'cells per block')
+print('Feature vector length:', len(X_train[0]))
+########################################################################################################################
+
+
+####################################################Train Classifier####################################################
+# Use a linear SVC
+svc = LinearSVC(C = 0.001)
+# Check the training time for the SVC
+t = time.time()
+svc.fit(X_train, y_train)
+t2 = time.time()
+print(round(t2-t, 2), 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+########################################################################################################################
+
+###########################################Proscess Image###############################################################
+
+def process_frame(img):
+    Boxes = []
+
+    colorspace = 'YCrCb'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+    orient = 11
+    pix_per_cell = 16
+    cell_per_block = 2
+    hog_channel = 'ALL'  # Can be 0, 1, 2, or "ALL"
+
+    ystart = 380
+    ystop = 490
+    scale = 1.0
+    Boxes.append(Utilities.find_cars(img, ystart, ystop, scale, colorspace, hog_channel, svc, None,
+                                orient, pix_per_cell, cell_per_block, None, None))
+    ystart = 400
+    ystop = 600
+    scale = 1.5
+    Boxes.append(Utilities.find_cars(img, ystart, ystop, scale, colorspace, hog_channel, svc, None,
+                                orient, pix_per_cell, cell_per_block, None, None))
+    ystart = 400
+    ystop = 660
+    scale = 2.0
+    Boxes.append(Utilities.find_cars(img, ystart, ystop, scale, colorspace, hog_channel, svc, None,
+                                orient, pix_per_cell, cell_per_block, None, None))
+
+    rectangles = [item for sublist in Boxes for item in sublist]
+
+    heatmap_img = np.zeros_like(img[:, :, 0])
+    heatmap_img = Utilities.add_heat(heatmap_img, rectangles)
+    heatmap_img = Utilities.apply_threshold(heatmap_img, 0.9)
+    labels = label(heatmap_img)
+    draw_img, rects = Utilities.draw_labeled_bboxes(np.copy(img), labels)
+    return draw_img
+
+####################################Try vehicle detection on test images################################################
+# test_images = glob.glob('./test_images/test*.jpg')
+#
+# fig, axs = plt.subplots(3, 2, figsize=(16,14))
+# fig.subplots_adjust(hspace = .004, wspace=.002)
+# axs = axs.ravel()
+#
+# for i, im in enumerate(test_images):
+#     axs[i].imshow(process_frame(mpimg.imread(im)))
+#     mpimg.imsave('output_images/test'+ str(i+1) + '.jpg',process_frame(mpimg.imread(im)))
+#
+#     axs[i].axis('off')
+#     plt.show()
+########################################################################################################################
+
+Video_Output_path = "output_video"
+if not os.path.isdir(Video_Output_path):
+    os.mkdir(Video_Output_path)
+
+input_path = "project_video.mp4"
+print(input_path)
+Video_Output_path = "output_video/" + "output_Project_video.mp4"
+clip1 = VideoFileClip(input_path)
+white_clip = clip1.fl_image(process_frame)
+white_clip.write_videofile(Video_Output_path, audio=False)
---
+++ Utilities.py
@@ -0,0 +1,244 @@
+import numpy as np
+import cv2
+from skimage.feature import hog
+import matplotlib.image as mpimg
+
+#################################lesson 12: Histograms of color#########################################################
+# Function to compute color histogram features
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    # Compute the histogram of the color channels separately
+    channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+    channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+    channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return hist_features
+########################################################################################################################
+
+
+#################################lesson 16: Spatial Bining of color#####################################################
+
+# Function to compute color histogram features
+# Pass the color_space flag as 3-letter all caps string
+def bin_spatial(img, size=(32, 32)):
+    # Use cv2.resize().ravel() to create the feature vector
+    features = cv2.resize(img, size).ravel()
+    # Return the feature vector
+    return features
+########################################################################################################################
+
+########################################lesson 20: scikit-image HOG#####################################################
+# Function to return HOG features and visualization
+def get_hog_features(img, orient, pix_per_cell, cell_per_block,
+                        vis=False, feature_vec=True):
+    # Call with two outputs if vis==True
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient,
+                                  pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block),
+                                  transform_sqrt=True,
+                                  visualise=vis, feature_vector=feature_vec)
+        return features, hog_image
+    # Otherwise call with one output
+    else:
+        features = hog(img, orientations=orient,
+                       pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block),
+                       transform_sqrt=True,
+                       visualise=vis, feature_vector=feature_vec)
+        return features
+########################################################################################################################
+
+
+########################################################################################################################
+# Function that takes an image, a list of bounding boxes,
+# and optional color tuple and line thickness as inputs
+# then draws boxes in that color on the output
+# Here is your draw_boxes function from the previous exercise
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # Make a copy of the image
+    imcopy = np.copy(img)
+    random_color = False
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        if color == 'random' or random_color:
+            color = (np.random.randint(0,255), np.random.randint(0,255), np.random.randint(0,255))
+            random_color = True
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+    # Return the image copy with boxes drawn
+    return imcopy
+########################################################################################################################
+
+###################################Lesson 22: Combine and Normalize Features############################################
+# Define a function to extract features from a list of image locations
+def extract_features(imgs, cspace='RGB', orient=9,
+                        pix_per_cell=8, cell_per_block=2, hog_channel=0):
+    # Create a list to append feature vectors to
+    features = []
+    # Iterate through the list of images
+    for file in imgs:
+        # Read in each one by one
+        image = mpimg.imread(file)
+        # apply color conversion if other than 'RGB'
+        if cspace != 'RGB':
+            if cspace == 'HSV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+            elif cspace == 'LUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+            elif cspace == 'HLS':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+            elif cspace == 'YUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+            elif cspace == 'YCrCb':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+        else: feature_image = np.copy(image)
+
+        # Call get_hog_features() with vis=False, feature_vec=True
+        if hog_channel == 'ALL':
+            hog_features = []
+            for channel in range(feature_image.shape[2]):
+                hog_features.append(get_hog_features(feature_image[:,:,channel],
+                                    orient, pix_per_cell, cell_per_block,
+                                    vis=False, feature_vec=True))
+            hog_features = np.ravel(hog_features)
+        else:
+            hog_features = get_hog_features(feature_image[:,:,hog_channel], orient,
+                        pix_per_cell, cell_per_block, vis=False, feature_vec=True)
+        # Append the new feature vector to the features list
+        features.append(hog_features)
+    # Return list of feature vectors
+    return features
+########################################################################################################################
+
+
+
+
+
+################################################find car function#######################################################
+# Define a single function that can extract features using hog sub-sampling and make predictions
+def find_cars(img, ystart, ystop, scale, cspace, hog_channel, svc, X_scaler, orient,
+              pix_per_cell, cell_per_block, spatial_size, hist_bins, show_all_Boxes=False):
+    # array of Boxes where cars were detected
+    Boxes = []
+
+    img = img.astype(np.float32) / 255
+
+    img_tosearch = img[ystart:ystop, :, :]
+
+    # apply color conversion if other than 'RGB'
+    if cspace != 'RGB':
+        if cspace == 'HSV':
+            ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2HSV)
+        elif cspace == 'LUV':
+            ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2LUV)
+        elif cspace == 'HLS':
+            ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2HLS)
+        elif cspace == 'YUV':
+            ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2YUV)
+        elif cspace == 'YCrCb':
+            ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2YCrCb)
+    else:
+        ctrans_tosearch = np.copy(img)
+
+    # rescale image if other than 1.0 scale
+    if scale != 1:
+        imshape = ctrans_tosearch.shape
+        ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1] / scale), np.int(imshape[0] / scale)))
+
+    # select colorspace channel for HOG
+    if hog_channel == 'ALL':
+        ch1 = ctrans_tosearch[:, :, 0]
+        ch2 = ctrans_tosearch[:, :, 1]
+        ch3 = ctrans_tosearch[:, :, 2]
+    else:
+        ch1 = ctrans_tosearch[:, :, hog_channel]
+
+    # Define blocks and steps as above
+    nxblocks = (ch1.shape[1] // pix_per_cell) + 1  # -1
+    nyblocks = (ch1.shape[0] // pix_per_cell) + 1  # -1
+    nfeat_per_block = orient * cell_per_block ** 2
+    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
+    window = 64
+    nblocks_per_window = (window // pix_per_cell) - 1
+    cells_per_step = 2  # Instead of overlap, define how many cells to step
+    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
+    nysteps = (nyblocks - nblocks_per_window) // cells_per_step
+
+    # Compute individual channel HOG features for the entire image
+    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    if hog_channel == 'ALL':
+        hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+        hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+
+    for xb in range(nxsteps):
+        for yb in range(nysteps):
+            ypos = yb * cells_per_step
+            xpos = xb * cells_per_step
+            # Extract HOG for this patch
+            hog_feat1 = hog1[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+            if hog_channel == 'ALL':
+                hog_feat2 = hog2[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+                hog_feat3 = hog3[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+                hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
+            else:
+                hog_features = hog_feat1
+
+            xleft = xpos * pix_per_cell
+            ytop = ypos * pix_per_cell
+
+            test_prediction = svc.predict(hog_features.reshape(1, -1))
+
+            if test_prediction == 1 or show_all_Boxes:
+                xbox_left = np.int(xleft * scale)
+                ytop_draw = np.int(ytop * scale)
+                win_draw = np.int(window * scale)
+                Boxes.append(
+                    ((xbox_left, ytop_draw + ystart), (xbox_left + win_draw, ytop_draw + win_draw + ystart)))
+
+    return Boxes
+########################################################################################################################
+
+#################################################Heat Map###############################################################
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap  # Iterate through list of bboxes
+#######################################################################################################################
+
+
+
+##########################################Apply Threshold###############################################################
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+########################################################################################################################
+
+
+#############################################Draw Boxes#################################################################
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    rects = []
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        rects.append(bbox)
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+    # Return the image and final rectangles
+    return img, rects
+########################################################################################################################
+
